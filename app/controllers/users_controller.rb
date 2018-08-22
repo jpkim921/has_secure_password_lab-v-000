7938b0deb37de980898f2062aa8c8c623da58f4f
Jayon Kim <jpkim921@gmail.com>
2018-08-22 03:58:45 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
 
   def new
-    @user = User.new
   end
 
   def create
     @user = User.create(user_params)
-
+    return redirect_to controller: 'users', action: 'new' unless @user.save
+    session[:user_id] = @user.id
+    redirect_to controller: 'welcome', action: 'home'
   end
-
-
 
   private
 
